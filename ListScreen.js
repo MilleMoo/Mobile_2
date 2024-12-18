1c49088db89bd9fb259f836361856571cfcfb6ea
MilleMoo <118721832+MilleMoo@users.noreply.github.com>
2024-12-18 20:53:14 +0700

    Update ListScreen.js

--- ListScreen.js
+++ ListScreen.js
@@ -1,43 +1,72 @@
 import React from "react";
-import { View, Text, StyleSheet, FlatList, TouchableOpacity, Alert} from "react-native";
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Alert, Image} from "react-native";
 
 const ListScreen = () => {
     const friends = [
-        {name: 'A',age: 15, relation: "school"},
-        {name: 'B',age: 20, relation: "Family"},
-        {name: 'C',age: 24, relation: "school"},
-        {name: 'D',age: 21, relation: "school"},
-        {name: 'E',age: 30, relation: "Family"},
-        {name: 'F',age: 20, relation: "school"},
-        {name: 'G',age: 20, relation: "Family"},
-        {name: 'H',age: 20, relation: "school"},
+        {name: 'Mom',status: "Mobile", time: "Sunday"},
+        {name: 'DadB',status: "Phone", time: "Yesterday"},
+        {name: 'mobile',status: "Mobile", time: "Sunday"},
+        {name: 'DadD',status: "Phone", time: "Sunday"},
+        {name: 'has',status: "Mobile", time: "Yesterday"},
+        {name: 'DadF',status: "Phone", time: "Friday"},
+        {name: 'eles',status: "Mobile", time: "Yesterday"},
+        {name: 'DadH',status: "Phone", time: "Friday"},
     ]
     return (
         <View style = {styles.container}>\
         <Text style = {{ 
-            fontSize : 30, 
+            fontSize: 50, 
+            font: "bold",
             //marginBottom : 30,
-            margin : 50,
+            margin: 10,
             color: "#F3FCFF",
-            }}>ListFriendName</Text>
+            justifyContent: "left",
+            width: "100%",
+            }}>Recent</Text>
         <FlatList
-            keyExtractor={friend => friend.name}
+            keyExtractor={(item, index )=> index.name}
             data={friends}
             renderItem={({ item }) => { 
                 return (
-                        <TouchableOpacity style={styles.button} onPress={() => Alert.alert("Calling","Calling to Friend "+ item.name)}>
+                    <TouchableOpacity style={styles.button} onPress={() => Alert.alert("Calling", "Calling to " + item.name)}>
                         <View style={{
-                            margin: 0.5,
-                            borderColor: "#01497c",
-                            backgroundColor: "#2c7da0",
-                            padding: 5,
-                            paddingLeft: 70,
-                            paddingRight: 70,
-                            borderWidth: 1.5,
-                            borderRadius: 10
+                            margin: 0,
+                            borderColor: "white",
+                            borderTopWidth: 0,
+                            borderLeftWidth: 0,
+                            borderRightWidth: 0,
+                            backgroundColor: "black",
+                            borderWidth: 1,
+                            width: "100%",
+                            flexDirection: "row",
+                            padding: 20,
+                            paddingLeft: 15,
                         }}>
-                            <Text style={styles.TextStyle}>Friend {item.name} - Age {item.age}</Text>
-                            <Text style={{ fontSize: 20,color: "#F3FCFF",textAlign : "center" }}>{item.relation}</Text>
+                            <Image source={{ uri: "https://cdn.marvel.com/u/prod/marvel/i/mg/6/80/66bb7142277fc/clean.jpg" }}
+                                style={{
+                                    width: 60,
+                                    height: 60,
+                                    borderRadius: 100,
+                                    borderWidth: 2,
+                                    borderColor: 'white',
+                                }}></Image>
+                            <View style={{
+                                paddingRight: "50%",
+                                marginLeft: 15,
+                                paddingRight: 100,
+                                width : "62%"
+                            }}>
+                                <Text style={{ 
+                                    fontSize : 25,
+                                    textAlign : "left",
+                                    color: "#F3FCFF",
+                                    marginBottom : 5,
+                                }}>{item.name} </Text>
+                                <Text style={{ fontSize: 15 , color: "gray"}}>{item.status}</Text>
+                            </View>
+                            <View style = {{ padding: 10,paddingTop: 20, width: 90 ,}}> 
+                                <Text style={{ textAlign: 'right', fontSize: 15,color: "#F3FCFF"}}>{item.time}</Text>
+                            </View>
                         </View>
                         </TouchableOpacity>
                 )
@@ -54,13 +83,13 @@ const styles = StyleSheet.create({
     TextStyle: {
         fontSize : 28,
         textAlign : "center",
-        color: "#F3FCFF",
+        color: "black",
     },
     container: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor : "#01497c",
+        backgroundColor : "black",
     }
 });
 
