4061e1bbce4ae677a35cba933d9c98354c1d71bb
MilleMoo <118721832+MilleMoo@users.noreply.github.com>
2024-12-25 20:38:36 +0700

    Update App.js

--- App.js
+++ App.js
@@ -5,36 +5,47 @@ import HomeScreen from "./src/screens/HomeScreen";
 import ComponentScreen from "./src/screens/ComponentScreen";
 import ListScreen from "./src/screens/ListScreen";
 import ModalScreen from "./src/screens/ModalScreen";
+import SwipeScreen from "./src/screens/SwipeScreen";
+import NewListScreen from "./src/screens/NewListScreen";
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Modal">
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{ title: "Home Screen" }}
+          options={{ title: "HomeScreen" }}
         />
         <Stack.Screen
           name="Component"
           component={ComponentScreen}
-          options={{ title: "Component Screen" }}
+          options={{ title: "ComponentScreen" }}
         />
         <Stack.Screen
           name="ListScreen"
           component={ListScreen}
-          options={{ title: "List Screen" }}
+          options={{ title: "ListScreen" }}
         />
         <Stack.Screen
           name="Modal"
           component={ModalScreen}
-          options={{ title: "Modal Screen" }}
+          options={{ title: "ModalScreen" }}
+        />
+        <Stack.Screen
+          name="Swipe"
+          component={SwipeScreen}
+          options={{ title: "SwipeScreen" }}
+        />
+        <Stack.Screen
+          name="NewList"
+          component={NewListScreen}
+          options={{ title: "NewListScreen" }}
         />
       </Stack.Navigator>
     </NavigationContainer>
-    
   );
 };
 
