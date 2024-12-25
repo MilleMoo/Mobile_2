e78c185dc980f589edf950c4b4206839b8764026
MilleMoo <118721832+MilleMoo@users.noreply.github.com>
2024-12-25 20:39:08 +0700

    Update HomeScreen.js

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,10 +1,22 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet ,Button} from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}) => {
     return (
         <View style={styles.container}>
-            <Text style={styles.TextStyle}>this is the home screen</Text>
+            <View style = {{ marginBottom: 20 }}>
+                <Button title="Go to List Demo"onPress={() => navigation.navigate("ListScreen")} color={"red"}></Button>
+            </View>
+            <View style = {{ marginBottom: 20 }}>
+                <Button title="Go to Modal Demo" onPress={() => navigation.navigate("Modal")}color={"blue"}></Button>
+            </View>
+            <View style = {{ marginBottom: 20 }}>
+                <Button title="Go to Swipe Demo" onPress={() => navigation.navigate("Swipe")}color={"green"}></Button>
+            </View>
+            <View style = {{ marginBottom: 20 }}>
+                <Button title="Go to NewList Demo" onPress={() => navigation.navigate("NewList")}color={"green"}></Button>
+            </View>
         </View>
     );
 };
@@ -16,7 +28,12 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
+        backgroundColor: "gray",
+        paddingTop: 10,
+        paddingRight: 20,
+        paddingBottom: 30,
+        paddingLeft: 40,
     }
 });
 
